d45848285ac6879f2a098b91308ede7f9625880f
Darshan Shah <shahdarshah1510@gmail.com>
2021-01-02 04:00:20 +0530

    Todo list application added with axios (JsonPlaceHolder).

--- src/App.js
+++ src/App.js
@@ -1,9 +1,72 @@
-function App() {
-  return (
-    <div className="App">
-      Hello Darshan!! How are you?
-         </div>
-  );
+import React,{Component} from 'react';
+import Todos from './components/Todos';
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
+//import {v4 as uuid} from "uuid";
+import { BrowserRouter as Router,Route } from "react-router-dom";
+import About from "./components/pages/About";
+import axios from "axios"
+
+class App extends Component {
+  state={
+    todos:[]
+  }
+
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10').then(
+      res=>this.setState({todos: res.data}))
+  }
+
+  //toggle complete
+  markComplete = (id) =>{
+    this.setState({
+      todo:this.state.todos.map(todo=>{
+        if(todo.id === id){
+          todo.completed=!todo.completed
+        }
+        return todo;
+      })
+    })
+  }
+
+  //delete todos
+  delTodo=(id)=>{
+
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(res=>this.setState({todos:[...this.state.todos.filter(todo=>(
+      todo.id!==id
+    ))]}));
+    
+  }
+
+  addTodo = (title) =>{
+    axios.post('https://jsonplaceholder.typicode.com/todos',{
+      title,
+      completed:false
+    }).then(res=>this.setState({todos:[...this.state.todos,res.data]}))
+    
+  }
+
+  render(){
+    const {todos}=this.state;
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header/>
+            <Route exact path="/" render={props=>(
+              <React.Fragment>             
+                <AddTodo addTodo={this.addTodo}/>
+                <Todos todos={todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+              </React.Fragment>
+            )}></Route>
+            <Route path="/about" component={About}></Route>
+           </div>
+        </div>
+      </Router>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/AddTodo.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export class AddTodo extends Component {
+
+    state={
+        title:''
+    }
+
+    onChange = (e) => this.setState({title:e.target.value});
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({title:''})
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit} style={{display: 'flex'}}>
+                <input type="text" 
+                name="title" 
+                value={this.state.title}
+                onChange={this.onChange}
+                placeholder="Add Todo..." style={{flex:'10',padding:'5px'}}/>
+            <input type="submit" value="submit" style={{flex:'1'}}/>
+            </form>
+        )
+    }
+}
+
+export default AddTodo
